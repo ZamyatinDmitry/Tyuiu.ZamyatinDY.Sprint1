9a1a58177ab462b7bf09afb18037533a86fd0029
Tyuiu.ZamyatinDY@outlook.com <dimazamatin18@gmail.com>
2023-09-23 00:06:33 +0500

    Task1

---
+++ Tyuiu.ZamyatinDY.Sprint1.Task1.V19.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ZamyatinDY.Sprint1.Task1.V19.Lib
+{
+    public class DataService : ISprint1Task1V19
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x + 10 * y) / (x * y);
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint1.Task1.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZamyatinDY.Sprint1.Task1.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZamyatinDY.Sprint1.Task1.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("277da179-a378-4f7c-8f41-1da18459b8d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
