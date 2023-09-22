7f946f02ddbf3c501b63640aa1a25fe82bd9ba57
Tyuiu.ZamyatinDY@outlook.com <dimazamatin18@gmail.com>
2023-09-22 23:53:52 +0500

    Add project files.

---
+++ Tyuiu.ZamyatinDY.Sprint1.Task0.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZamyatinDY.Sprint1.Task0.V29.Lib;
+
+namespace Tyuiu.ZamyatinDY.Sprint1.Task0.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Замятин Д. Ю. | ИИПБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Замятин Дмитрий Юрьевич | ИИПБ-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 2 * 7 + 9 * 2           *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2 * 7 + 9 * 2                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+        }
+    }
+}
